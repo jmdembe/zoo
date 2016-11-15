58f55e5d0a2a0c37169e6b5387b43a618a7d9315
Jessica Dembe <jmdembe@gmail.com>
2016-11-15 15:30:57 -0500

    Created constructors for red panda, animal, and cheetah as well as created an index file to open

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,39 @@
 (function() {
     'use strict';
 
+
+    /**
+     * Creates a new animal constructor
+     * @param {Object} species Constructs animals in the zoo
+     */
+    function Animal(name, dob) {
+        console.log('Which animal is this?', name);
+        this.name = name || 'anon';
+        this.dob = dob;
+    }
+
+    Animal.prototype.whatAmI = function whatAmI () {
+        console.log('I am a '+ this.species);
+    }
+
+    /**
+     * Creates a new red panda
+     * @param {[type]} name [description]
+     */
+    function RedPanda(name) {
+        this.name = name;
+    }
+
+    /**
+     * Creates a new cheetah
+     * @param {[type]} name [description]
+     */
+    function Cheetah(name) {
+        this.name = name;
+    }
+    
+    var exhibit1 = new Animal('Fluffy', 5);
+    var babyPanda = new RedPanda('Fluffy');
+
+
 }());
