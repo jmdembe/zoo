ffd7ac43713ebf94c9f1a4ff589c03886598781a
Jessica Dembe <jmdembe@gmail.com>
2016-11-16 14:11:55 -0500

    Created tests for Animal constructor

--- test/specs/data.specs.js
+++ test/specs/data.specs.js
@@ -3,12 +3,24 @@
 
     var expect = chai.expect;
 
-    describe('Animal constructor', function (){
+
+    describe('Animal constructor', function () {
         it('Should be a function', function () {
-            expect(Animal).to.be.a('function');
+            expect(window.zoo.Animal).to.be.a('function');
         })
-        it('Should be a constructor', function (){
-            expect(Animal).to.be.an.instanceof('constructor');
+        it('Should create an animal', function (){
+            var fluffy = new window.zoo.Animal('Fluffy', 2000);
+            expect(fluffy).to.be.an.instanceof(window.zoo.Animal);
+        })
+
+        it('The name argument should be a string', function () {
+            var spots = new window.zoo.Animal('Spots', 2000);
+            expect(spots.name).to.be.a('String');
+        })
+
+        it('The dob argument should be a number', function (){
+            var ace = new window.zoo.Animal('Ace', 2008);
+            expect(ace.dob).to.be.a('Number');
         })
     });
 
